fd4175a13712ed596c5803fcdbb944bfee5ca73b
樋口雅拓 <mahiguch@hotmail.co.jp>
2025-08-24 15:55:44 +0900

    remove web and function for rebuild

--- web/src/components/layout/index.ts
+++
@@ -1,8 +0,0 @@
-export { Layout, type LayoutProps } from './Layout';
-export { Header } from './Header';
-export { Footer } from './Footer';
-export {
-  Breadcrumb,
-  type BreadcrumbProps,
-  type BreadcrumbItem,
-} from './Breadcrumb';
